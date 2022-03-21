777a761a6d6347326c4f1353c5108a665c23b64a
nospyrin <mohammedzuhair527@gmail.com>
2022-03-21 17:35:27 +0200

    add styles for page layout & extract variables to separate file

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,13 +1,18 @@
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 function Home() {
     return (
-        <>
+        <main className="main">
             <Header label="Product List">
                 <button className="button primary">Add</button>
                 <button className="button danger">Mass Delete</button>
             </Header>
-        </>
+
+            <article className="content"></article>
+
+            <Footer />
+        </main>
     );
 }
 
---
+++ client/src/components/Footer.js
@@ -0,0 +1,9 @@
+import './Footer.scss';
+
+function Footer() {
+    return (
+        <footer className="footer">Scandiweb Test Assignment</footer>
+    );
+}
+
+export default Footer;
