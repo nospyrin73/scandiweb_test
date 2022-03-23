aa0f0ba50738e056bce123ce39d5ca1eb73d9cb6
nospyrin <mohammedzuhair527@gmail.com>
2022-03-23 17:22:48 +0200

    add front-end form validation

--- client/src/AddProduct.js
+++ client/src/AddProduct.js
@@ -33,6 +33,8 @@ function AddProduct() {
     }
 
      function save() {
+        validate();
+
         const { sku, name, price, ...special } = values;
 
         let formData = new FormData();
@@ -42,7 +44,7 @@ function AddProduct() {
         formData.append('price', price);
         formData.append('type', type);
         
-        switch(type) {
+        switch (type) {
             case 'DVD':
                 formData.append('size', special.size);
                 break;
@@ -61,6 +63,40 @@ function AddProduct() {
             method: 'POST',
             body: formData
         });
+    }
+
+    function validate() {
+        const { sku, name, price, ...special } = values;
+        
+        const expectNonEmpty = [sku, name, price];
+        const expectNonNaN = [price];
+
+        switch (type) {
+            case 'DVD':
+                expectNonEmpty.push(special.size);
+                expectNonNaN.push(special.size);
+                break;
+            case 'Furniture':
+                expectNonEmpty.push(special.height, special.weight, special.length);
+                expectNonNaN.push(special.height, special.weight, special.length);
+                break;
+            case 'Book':
+                expectNonEmpty.push(special.weight);
+                expectNonNaN.push(special.weight);
+                break;
+            default:
+        }
+
+        if (expectNonEmpty.includes("")) {
+            console.log('expectNonEmpty found an empty: ');
+            console.log(expectNonEmpty); 
+        }
+
+        if (expectNonNaN.some( val => isNaN(val) )) {
+            console.log('expectNonNaN found a nan: ');
+            console.log(expectNonNaN);
+        }
+
     }
 
     return (
