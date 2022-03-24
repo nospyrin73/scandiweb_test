55e1487faeea97521537964f19df376cb86b69d5
nospyrin <mohammedzuhair527@gmail.com>
2022-03-24 13:34:34 +0200

    fix Request, Response namespace hierarchy

--- server/utils/Request.php
+++ server/utils/Request.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace App\Utils;
 
 class Request {
     private array $payload;
--- server/utils/Response.php
+++ server/utils/Response.php
@@ -2,7 +2,7 @@
 
 declare(strict_types = 1);
 
-namespace App;
+namespace App\Utils;
 
 class Response {
     public function type(string $contentType): self {
