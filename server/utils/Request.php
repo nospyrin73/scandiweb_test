091b1d3510fa70b8bafaa21ff687dc9a46dcb551
nospyrin <mohammedzuhair527@gmail.com>
2022-03-24 13:09:59 +0200

    add ability to read different types of payloads and populate them inside request object

---
+++ server/utils/Request.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App;
+
+class Request {
+    private array $payload;
+
+    public function __construct(
+        private string $requestUri,
+        private string $requestMethod,
+        private string $contentType
+    ) {
+        $this->payload = null;
+    }
+
+    public function populatePayload(): Request {
+        if ($this->requestMethod === 'GET') {
+            $this->payload = $_GET;
+        } else if ($this->requestMethod === 'POST') {
+            if (preg_match("#application/json#", $this->contentType)) {
+                $json = file_get_contents('php://input');
+
+                $this->payload = json_decode($json);
+            } else if (
+                preg_match("#application/x-www-form-urlencoded#", $this->contentType)
+                ||
+                preg_match("#multipart/form-data#", $this->contentType)
+            ) {
+                $this->payload = $_POST;
+            }
+        }
+
+        return $this;
+    }
+}
