e79b8da97e5adccedb24df3d83154555d1010243
nospyrin <mohammedzuhair527@gmail.com>
2022-03-29 21:23:26 +0200

    add getter methods

--- server/Product/Product.php
+++ server/Product/Product.php
@@ -4,7 +4,7 @@ declare(strict_types = 1);
 
 namespace App\Product;
 
-use App\Database\Query;
+use App\Database\Database;
 
 abstract class Product {
     protected string $name;
@@ -18,21 +18,37 @@ abstract class Product {
     ) {
     }
 
+    public function getSku(): string {
+        return $this->sku;
+    }
+
     public function setName(string $name): void {
         $this->name = $name;
+    }
+
+    public function getName(): string {
+        return $this->name;
     }
 
     public function setPrice(float $price): void {
         $this->price = $price;
     }
 
+    public function getPrice(): float {
+        return $this->price;
+    }
+
     public function setType(string $type) {
         $this->type = $type;
     }
 
+    public function getType(): string {
+        return $this->type;
+    }
+
     abstract public function insert();
 
-    abstract public function delete();
+    abstract public function delete(Database $db): int;
 
     public function toMap(): array {
         return [
